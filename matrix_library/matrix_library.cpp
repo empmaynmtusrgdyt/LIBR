11ec14416717e39a9994197cfe47ee967672a7dc
empmaynmtusrgdyt <leammax@yandex.ru>
2025-09-04 19:03:00 +0300

    three new functions for matrix_library

--- matrix_library/matrix_library.cpp
+++ matrix_library/matrix_library.cpp
@@ -32,3 +32,69 @@ void freeMatrix(int **matrix, int rows) {
     }
     delete[] matrix; 
 }
+
+int **loadMatrix(string filename, int rows, int cols) {
+    ifstream file(filename);
+
+    if (!file) {
+        cout << "Ошибка открытия файла!" << endl;
+        return nullptr; 
+    }
+
+    int **matrix = new int *[rows];
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = new int[cols]{};
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (!(file >> matrix[i][j])) break;
+        }
+    }
+    file.close();
+    return matrix;
+}
+
+void printMatrix(int**matrix, string filename, int rows, int cols){
+    ofstream file(filename);
+    if (!file) { // Проверяем, открылся ли файл
+        cout << "Ошибка открытия файла!" << endl;
+        return; //
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            file << setw(3) << matrix[i][j];
+        }
+        file << endl;
+    }
+
+    file.close();
+}
+
+int **loadMatrixWithSize(string filename, int &rows, int &cols){
+    ifstream file(filename);
+
+    if (!file) {
+        cout << "Ошибка открытия файла!" << endl;
+        return nullptr;
+    }
+
+    if (!(file >> rows >> cols)) {
+        cout << "Ошибка считывания размеров матрицы!" << endl;
+        return nullptr;
+    }
+
+    int **matrix = new int *[rows];
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = new int[cols]{};
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (!(file >> matrix[i][j])) break;
+        }
+    }
+
+    return matrix;
+}
--- matrix_library/matrix_library.h
+++ matrix_library/matrix_library.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 
 using namespace std;
 
@@ -9,3 +10,9 @@ int **createMatrix(int rows, int columns);
 void printMatrix(int **matrix, int rows, int columns);
 
 void freeMatrix(int **matrix, int rows);
+
+int **loadMatrix(string filename, int rows, int cols);
+
+void printMatrix(string filename, int rows, int cols);
+
+int **loadMatrixWithSize(string filename, int &rows, int &cols);
